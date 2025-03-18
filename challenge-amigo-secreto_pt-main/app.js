7cbfadcad2f1b9e9a32aa8702671ca6cc506226b
Kauan Soares <kauansoares.me14@gmail.com>
2025-03-18 19:52:23 -0300

    Implementa função para atualizar lista de amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,15 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    const nomeAmigo = document.getElementById('amigo').value.trim();
+  
+    if (nomeAmigo !== '') {
+      amigos.push(nomeAmigo);
+      const li = document.createElement('li');
+      li.textContent = nomeAmigo;
+      document.getElementById('listaAmigos').appendChild(li); 
+      document.getElementById('amigo').value = '';
+    } else {
+      alert("Por favor, insira um nome válido!");
+    }
+  }
